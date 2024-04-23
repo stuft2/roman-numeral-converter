b5a68d7cda6e4ae6ca37609d7a17a3cf4ee753e8
Spencer Tuft <stuft2@byu.edu>
2024-04-22 22:41:21 -0600

    ci: fix lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,9 @@ module.exports = {
       files: ['src/**/*.ts', 'test/**/*.ts'],
       extends: 'love',
       parserOptions: {
-        project: 'tsconfig.eslint.json'
+        project: './tsconfig.eslint.json'
       }
     }
   ],
+  ignorePatterns: ['bin/cli.js', 'dist/**/*.js'],
 }
